4c2085fb7cf678707727c14847074b2ccb3c3cd3
Shobhit Singh <shobhit268033@gmail.com>
2026-06-06 22:18:50 +0530

    Implement maximum subarray solution in Java

---
+++ Week_1/day2/MaximumSubarray.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int max=nums[0];
+        int currsum=nums[0];
+        for(int i=1;i<nums.length;i++){
+            currsum=Math.max(nums[i],currsum+nums[i]);
+            max=Math.max(max,currsum);
+            
+        }
+        return max;
+        
+    }
+}
