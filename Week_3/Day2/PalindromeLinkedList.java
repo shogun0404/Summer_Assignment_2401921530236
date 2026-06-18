1b7823b9cec78899542ee8084df89024a88c41a7
Shobhit Singh <shobhit268033@gmail.com>
2026-06-18 16:58:21 +0530

    Implement palindrome check for linked list

---
+++ Week_3/Day2/PalindromeLinkedList.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        Stack<Integer> stack = new Stack<>();
+        ListNode curr = head;
+        while(curr != null){
+            stack.push(curr.val);
+            curr = curr.next;
+        }
+        curr = head;
+        while(curr != null){
+            if(curr.val != stack.pop()){
+                return false;
+            } 
+            curr = curr.next;
+        }
+        return true;
+    }
+}
