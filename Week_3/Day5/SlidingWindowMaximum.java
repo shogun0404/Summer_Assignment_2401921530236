139993b505f15819ec6cae448a7c06d001840317
Shobhit Singh <shobhit268033@gmail.com>
2026-06-20 00:43:08 +0530

    Implement Sliding Window Maximum algorithm

---
+++ Week_3/Day5/SlidingWindowMaximum.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        Deque<Integer> dq = new LinkedList<>();
+        int[] ans = new int[nums.length - k + 1];
+        
+        for(int i = 0 ; i < nums.length ; i++ ){
+            
+            while(!dq.isEmpty() && dq.peekFirst() <= i - k){
+                dq.pollFirst();
+            }
+            
+            while(!dq.isEmpty() && nums[dq.peekLast()] < nums[i] ){
+                dq.pollLast();
+            }
+            dq.addLast(i);
+
+            if(i >= k-1 ){
+                ans[i - k + 1 ] = nums[dq.peekFirst()];
+            }
+
+        }
+        return ans;
+    }
+}
