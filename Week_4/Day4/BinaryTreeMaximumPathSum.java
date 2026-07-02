2bccd35e3fc633e048ad2db712db27fbc1c60709
Shobhit Singh <shobhit268033@gmail.com>
2026-07-02 21:44:01 +0530

    Implement maxPathSum for binary tree maximum path sum

---
+++ Week_4/Day4/BinaryTreeMaximumPathSum.java
@@ -0,0 +1,17 @@
+class Solution {
+    int max = Integer.MIN_VALUE;
+    public int maxPathSum(TreeNode root) {
+        help(root);
+        return max;
+    }
+    int help(TreeNode root ){
+        if(root==null)return 0;
+        
+        int left =  Math.max( 0 , help(root.left) );
+        int right = Math.max( 0 , help(root.right) );
+        max = Math.max( max , left + right + root.val);
+
+
+        return root.val + Math.max(left , right); 
+    }
+}
